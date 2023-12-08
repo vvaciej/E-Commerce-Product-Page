1f0768f476f720c18dd11b30c889e22a2bffa2aa
Maciek Skokowski <maciejskok2004@gmail.com>
2023-12-08 16:03:29 +0100

    all done // check mobile

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+// menu mobile section
+
 const menuBtn = document.querySelector('.menu-icon');
 const closeBtn = document.querySelector('.close-menu-icon');
 const menuContainer = document.querySelector('.menu');
@@ -22,14 +24,17 @@ document.addEventListener('click', closeMenuByOutside);
 closeBtn.addEventListener('click', menuContainerFunction);
 menuBtn.addEventListener('click', menuContainerFunction);
 
+// quantity, submit section
+
 const quantityText = document.querySelector('.quantity-text');
+const submitDiv = document.querySelector('.submit-div');
+const quantityCartDiv = document.querySelector('.quantity-cart-div');
 
 let quantity = 1;
 
 function quantityFunction (e) {
   const quantityMinus = document.querySelector('.quantity-minus');
   const quantityPlus = document.querySelector('.quantity-plus');
-  const submitDiv = document.querySelector('.submit-div');
   if (e.target === quantityPlus) {
     quantity++;
   } else if (e.target === quantityMinus) {
@@ -44,7 +49,6 @@ function quantityFunction (e) {
   quantityText.textContent = quantity;
   updateCost(quantity);
   if (submitDiv.contains(e.target)) {
-    console.log(submitDiv);
     updateQuantityCart(quantity);
   }
 }
@@ -63,7 +67,6 @@ function updateCost (quantity) {
 }
 
 function updateQuantityCart (quantity) {
-  const quantityCartDiv = document.querySelector('.quantity-cart-div');
   const quantityCart = document.querySelector('.quantity-cart');
 
   quantityCartDiv.classList.add('active');
@@ -71,3 +74,215 @@ function updateQuantityCart (quantity) {
 }
 
 document.addEventListener('click', quantityFunction);
+
+// preview photo section
+
+const imgPreview = document.querySelectorAll('.img-preview');
+const imgThumbnail = document.querySelectorAll('.img-thumbnail');
+
+const previewImg = [
+  {
+    firstPhoto: {
+      firstPreview: imgPreview[0],
+      firstThumbnail: imgThumbnail[0],
+    }
+  },
+  {
+    secondPhoto: {
+      secondPreview: imgPreview[1],
+      secondThumbnail: imgThumbnail[1],
+    }
+  },
+  {
+    thirdPhoto: {
+      thirdPreview: imgPreview[2],
+      thirdThumbnail: imgThumbnail[2],
+    }
+  },
+  {
+    fourthPhoto: {
+      fourthPreview: imgPreview[3],
+      fourthThumbnail: imgThumbnail[3],
+    }
+  },
+]
+
+let selectedPreviewIndex = 0;
+const allThumbnail = document.querySelectorAll('.img-thumbnail');
+
+function hideAllPhotos() {
+  previewImg.forEach((photo) => {
+    const key = Object.keys(photo)[0];
+    const values = Object.values(photo[key]);
+    values.forEach(e => {
+      e.classList.remove('active');
+    })
+  })
+}
+
+function showSelectedPhoto(selectedPreviewIndex) {
+  const selectedPreview = previewImg[selectedPreviewIndex];
+  const key = Object.keys(selectedPreview);
+  const values = Object.values(selectedPreview[key]);
+  values.forEach(e => {
+    e.classList.add('active');
+  })
+}
+
+function changePhoto (selectedPreviewIndex) {
+  hideAllPhotos();
+  showSelectedPhoto(selectedPreviewIndex);
+}
+
+allThumbnail.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', () => {
+    selectedPreviewIndex = index;
+    changePhoto(selectedPreviewIndex);
+  })
+})
+
+// fullscreen photo preview section
+
+const fullscreenPhotoClose = document.querySelector('.close-fullscreen-btn');
+const fullscreenContainer = document.querySelector('.photo-fullscreen');
+
+function showFullscreenPhoto(e) {
+  const html = document.querySelector('html');
+  
+  fullscreenContainer.classList.add('active');
+  html.classList.add('opacity');
+}
+
+imgPreview.forEach((preview) => {
+  preview.addEventListener('click', showFullscreenPhoto);
+});
+
+fullscreenPhotoClose.addEventListener('click', function () {
+  fullscreenContainer.classList.remove('active');
+  html.classList.remove('opacity');
+});
+
+const imgPreviewFullscreen = document.querySelectorAll('.img-preview-fullscreen');
+const imgThumbnailFullscreen = document.querySelectorAll('.img-thumbnail-fullscreen');
+
+
+const allSlidesFullscreen = [
+  {
+    firstPhoto: {
+      firstPreview: imgPreviewFullscreen[0],
+      firstThumbnail: imgThumbnailFullscreen[0],
+    }
+  },
+  {
+    secondPhoto: {
+      secondPreview: imgPreviewFullscreen[1],
+      secondThumbnail: imgThumbnailFullscreen[1],
+    }
+  },
+  {
+    thirdPhoto: {
+      thirdPreview: imgPreviewFullscreen[2],
+      thirdThumbnail: imgThumbnailFullscreen[2],
+    }
+  },
+  {
+    fourthPhoto: {
+      fourthPreview: imgPreviewFullscreen[3],
+      fourthThumbnail: imgThumbnailFullscreen[3],
+    }
+  },
+]
+
+let selectedPreviewIndexFullscreen = 0;
+
+function hideAllPhotosFullscreen() {
+  allSlidesFullscreen.forEach((photo) => {
+    const key = Object.keys(photo)[0];
+    const values = Object.values(photo[key]);
+    values.forEach(e => {
+      e.classList.remove('active');
+    })
+  })
+}
+
+function showSelectedPhotoFullscreen(selectedPreviewIndexFullscreen) {
+  const selectedPreview = allSlidesFullscreen[selectedPreviewIndexFullscreen];
+  const key = Object.keys(selectedPreview);
+  const values = Object.values(selectedPreview[key]);
+  values.forEach(e => {
+    e.classList.add('active');
+  })
+}
+
+function changePhotoFullscreen (selectedPreviewIndexFullscreen) {
+  hideAllPhotosFullscreen();
+  showSelectedPhotoFullscreen(selectedPreviewIndexFullscreen);
+}
+
+imgThumbnailFullscreen.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', () => {
+    selectedPreviewIndexFullscreen = index;
+    changePhotoFullscreen(selectedPreviewIndexFullscreen);
+  })
+})
+
+function controlPhotoFullscreen (e) {
+  const prevPhotoFullscreen = document.querySelector('.control-btn-previous');
+  const nextPhotoFullscreen = document.querySelector('.control-btn-next');
+  if (e.target === prevPhotoFullscreen) {
+    selectedPreviewIndexFullscreen--;
+    if (selectedPreviewIndexFullscreen < 0) {
+      selectedPreviewIndexFullscreen = 3;
+    }
+    changePhotoFullscreen(selectedPreviewIndexFullscreen);
+  } else if (e.target === nextPhotoFullscreen) {
+    selectedPreviewIndexFullscreen++;
+    if (selectedPreviewIndexFullscreen > 3) {
+      selectedPreviewIndexFullscreen = 0;
+    }
+    changePhotoFullscreen(selectedPreviewIndexFullscreen);
+  }
+} 
+
+document.addEventListener('click', controlPhotoFullscreen);
+
+// basket section
+const clearBasketBtn = document.querySelector('.cart-bill-delete-btn');
+const emptyCart = document.querySelector('.empty-cart');
+const filledCart = document.querySelector('.filled-cart');
+const basketBillTotalText = document.querySelector('.cart-bill-total-cost');
+const basketBillQuantityText = document.querySelector('.cart-bill-quantity');
+
+document.addEventListener('click', function (e) {
+  const basketBtn = document.querySelector('.cart-icon');
+  const basketContainer = document.querySelector('.basket-container');
+  const checkIsValidTargetToClose = !basketContainer.contains(e.target) && !e.target.contains(basketBtn);
+
+  if (e.target === basketBtn) {
+    basketContainer.classList.toggle('active');
+  }
+  if (checkIsValidTargetToClose) {
+    basketContainer.classList.remove('active');
+  }
+});
+
+function basketBill () {
+  emptyCart.classList.remove('active');
+  filledCart.classList.add('active');
+  basketBillTotalText.textContent = '$' + (costOfSneakers * quantity).toFixed(2);
+  basketBillQuantityText.textContent = quantity;
+}
+
+submitDiv.addEventListener('click', basketBill);
+
+function basketClearAll () {
+  emptyCart.classList.add('active');
+	filledCart.classList.remove('active');
+  quantityCartDiv.classList.remove('active');
+} 
+
+clearBasketBtn.addEventListener('click' , basketClearAll);
+
+// author vvaciej
+
+
